d39d07c997be22034de306b2ccafc90b1cad2ced
Anuj-jnv <anujk534208@gmail.com>
2024-06-15 16:01:43 +0530

    LoginPage Updated

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -105,7 +105,7 @@ const Login = () => {
         <img
           src="https://images.unsplash.com/photo-1485095329183-d0797cdc5676?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
-          className="h-[100rem] w-[100%] xl:block md:hidden sm:hidden rounded-lg"
+          className="login-background flex items-center justify-center bg-white p-8 rounded-lg shadow-md w-full max-w-md"
           
         />
       </section>
